2f9ab8958e2399b1d3ca623ee8cc0931d35b4516
Vedant Rokde <vedant24rokde@gmail.com>
2022-10-30 11:50:23 +0530

    Final commit

---
+++ android/app/src/main/kotlin/com/code/quote/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.code.quote
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/quote_card.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'quote.dart';
+
+class QuoteCard extends StatelessWidget {
+  final Quote quote;
+  final Function onDelete;
+
+  const QuoteCard({
+    Key? key,
+    required this.quote,
+    required this.onDelete
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: const EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0),
+      child: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              quote.text,
+              style: TextStyle(
+                fontSize: 18.0,
+                color: Colors.grey[600],
+              ),
+            ),
+            const SizedBox(height: 6.0),
+            Text(
+              quote.author,
+              style: TextStyle(
+                fontSize: 14.0,
+                color: Colors.grey[800],
+              ),
+            ),
+            const SizedBox(height: 6.0),
+            TextButton.icon(
+              onPressed: () => onDelete(),
+              label: const Text('Delete Quote'),
+              icon: const Icon(Icons.delete),
+              style: TextButton.styleFrom(
+                  primary: Colors.grey[800]
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
